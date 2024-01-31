d74249a15ef6e8688b3e351cfd9a5914c97c630a
Richard <rbraul@sintad.pe>
2024-01-31 00:04:59 -0500

    feat: agregar list y set demos

---
+++ src/other/two_collections/ListDemo.java
@@ -0,0 +1,91 @@
+package other.two_collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListDemo {
+
+
+//  * List es parte del framework de colecciones
+
+//  * Representa una:
+//  * Colección ordenada
+//  * Puede contener duplicados
+//  * Acceso mediante índices (posiciones)
+
+    public static void main(String[] args){
+//      * La razón por la cual se está usando un ArrayList
+//      * es porque como es una interfaz, no puede ser instanciada
+//      * directamente.
+
+//      * Por tanto se necesita usar una implementación concreta
+//      * para crear una instancia
+
+        List<String> list = new ArrayList<>();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add(0, "firstElement");
+
+        System.out.println(list);
+
+//      Agregar multiples items
+        Collections.addAll(list, "a", "b", "c");
+
+//      Obtener items
+        String secondList = list.get(2);
+        System.out.println(secondList);
+
+//      Setear items
+        list.set(0, "🤡");
+        System.out.println(list);
+
+//      Removiendo
+
+        list.remove(2);
+        System.out.println(list);
+
+//      Obtener index
+
+        int indexClown = list.indexOf("🤡");
+        int lastIndexOfA = list.lastIndexOf("a");
+        System.out.println(indexClown);
+
+//      Obtener una parte del list
+
+        List<String> newList = list.subList(2, 5);
+        System.out.println(newList);
+
+//      * Ejercicio:
+
+//        * Crea una lista de números enteros y luego:
+//
+//        * Ordena la lista de menor a mayor.
+//        * Imprime la lista ordenada.
+
+        List<Integer> listIntegers = new ArrayList<>();
+
+        Collections.addAll(listIntegers, 1,9,2,20,4,8);
+
+        System.out.println(listIntegers);
+
+        for (int i=0; i < listIntegers.size(); i++){
+            for(int j=i+1; j< listIntegers.size(); j++){
+                if (listIntegers.get(i) > listIntegers.get(j)){
+                    int temp = listIntegers.get(i);
+                    listIntegers.set(i, listIntegers.get(j));
+                    listIntegers.set(j, temp);
+                }
+            }
+        }
+
+//        Collections.sort(listIntegers);
+
+
+        System.out.println(listIntegers);
+
+    }
+
+}
---
+++ src/other/two_collections/SetDemo.java
@@ -0,0 +1,83 @@
+package other.two_collections;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo {
+
+
+    public static void main(String[] args){
+        //  * Set es una colección que:
+
+        //  * No permite elementos duplicados
+        //  * No tiene un orden definido
+
+        //  * Con respecto a la implementación de HashSet
+
+        //  * Se dice que es un conjunto de Hash, se refiere a
+        //  * un algoritmo de conversión de datos en un valor
+        //  * entero único - Tabla Hash
+
+        Set<String> conjuntoHash = new HashSet<>();
+
+        conjuntoHash.add("z");
+        conjuntoHash.add("b");
+        conjuntoHash.add("f");
+        conjuntoHash.add("e");
+        conjuntoHash.add("q");
+        conjuntoHash.add("q");
+        conjuntoHash.add("c");
+        conjuntoHash.add("z");
+        conjuntoHash.add("a");
+
+        System.out.println(conjuntoHash);
+
+        boolean isThereLetterZ = conjuntoHash.contains("z");
+        System.out.println(isThereLetterZ);
+
+
+//      * Implementacion TreeSet
+
+//      * Mantiene un orden ascendente - menor a mayor -  de manera natural tanto alfabetico
+//      * como numérico
+
+        Set<String> conjuntoTree = new TreeSet<>();
+
+        conjuntoTree.add("a");
+        conjuntoTree.add("z");
+        conjuntoTree.add("b");
+        conjuntoTree.add("f");
+        conjuntoTree.add("e");
+        conjuntoTree.add("q");
+        conjuntoTree.add("q");
+        conjuntoTree.add("c");
+        conjuntoTree.add("z");
+        conjuntoTree.add("a");
+        conjuntoTree.add("a");
+
+//      * Se va a ordenar
+        System.out.println(conjuntoTree);
+
+
+//      * Implementacion LinkedHashSet
+
+//      * Es similar a un List
+
+//      * Pero, los elementos del conjunto se encuentran en el orden
+//      * que se insertan y no admite duplicados.
+
+        Set<String> conjuntoLinked = new LinkedHashSet<>();
+
+        conjuntoLinked.add("a");
+        conjuntoLinked.add("z");
+        conjuntoLinked.add("b");
+        conjuntoLinked.add("c");
+        conjuntoLinked.add("c");
+
+        System.out.println(conjuntoLinked);
+
+
+    }
+}
